6417409eb2342bf95589698d8a010e1d2a716bf0
agape1225 <scg9268@gmail.com>
2024-02-04 22:50:15 +0900

    Update Address.java

--- chapter 1/src/Address.java
+++ chapter 1/src/Address.java
@@ -3,6 +3,7 @@ public class Address {
     private String address2;
     private String zipcode;
 
+    //데이터의 무결성을 위해 생성자 생성
     public Address(String address1, String address2, String zipcode){
         this.address1 = address1;
         this.address2 = address2;
