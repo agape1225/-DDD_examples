3334283ff157e562538758583cac7c345047b32d
agape1225 <scg9268@gmail.com>
2024-02-07 23:03:16 +0900

    Update Test.java

--- chapter 1/src/Test.java
+++ chapter 1/src/Test.java
@@ -1,8 +1,9 @@
-
-Member member = memberRepository.findById(orderId);
-List<Order> orders = orderRepository.findByOrderer(orderId);
-List<OrderView> dtos = orders.stream().map(order ->{
-    ProductId prodiId = order.getOrderLines().get().getProductId();
-    Product product = productRepository.findById(prodId);
-    return new OrderView(order, member, product);
-}).collect(toList());
+class Test{
+    Specification<MemberData> spec = SpecBuilder.builder(MemberData.class)
+            .ifTrue(searchRequest.isOnlyNotBlocked(),
+                    () -> MemberDataSpecs.nonBlocked())
+            .ifHasText(searchRequest.getName(),
+                    name -> MemberDataSpecs.nameLike(sear chRequest.getName()))
+            .toSpec();
+    List<MemberData> result = memberDataDao.findAll(spec, PageRequest.of(0, 5));
+}
